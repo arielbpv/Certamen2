7c1609472cfcfe0b08e67d4d94121e5107a12e72
VaS <ariel170497@gmail.com>
2021-12-15 23:49:21 -0300

    asd
    asdasdasd

--- Certamen2/Certamen2ext.go
+++ Certamen2/Certamen2ext.go
@@ -1,47 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 type cajeros struct {
-	atencionCa *chan int
+	name int
 }
 
-func newCajeros(atencionCa *chan int) *cajeros {
-	return &cajeros{atencionCa: atencionCa}
+func newCajeros(name int) *cajeros {
+	return &cajeros{name: name}
 }
 
 func (c *cajeros) atencion() {
-	fmt.Println("Nuevo cliente entrando al banco")
-	for {
+	fmt.Println("está siendo atendido")
+	/*for {
 		dirige := <-*c.atencionCa
 		fmt.Print("El cliente: ", dirige)
 		fmt.Println("se dirige con el cajero")
-	}
+	}*/
+
 }
 
 type clientes struct {
-	atencionCli *chan int
-	terminado   *chan bool
+	name              int
+	TiempoAtencionCli int
+	terminado         chan bool
 }
 
-func newClientes(atencionCli *chan int, terminado *chan bool) *clientes {
-	return &clientes{atencionCli: atencionCli, terminado: terminado}
+func newClientes(name int, TiempoAtencionCli int, terminado chan bool) *clientes {
+	return &clientes{name: name, TiempoAtencionCli: TiempoAtencionCli, terminado: terminado}
 }
 
-func (cli *clientes) nuevo(max int) {
-	fmt.Println("Nueva atención")
-	for i := 0; i < max; i++ {
-		fmt.Println("El cliente ", i)
-		fmt.Println("está siendo atendido")
-		*cli.atencionCli <- i
+func (cli *clientes) nuevo(nclientes int, clienteSlice *[]*clientes, max int) {
+	var personasNew = rand.Intn(2)
+	for i := 0; i < personasNew; i++ {
+		if len(*clienteSlice) < 20 {
+			fmt.Println("Nuevo cliente entrando al banco")
+			var demora = rand.Intn(10)
+			var terminar = make(chan bool)
+			*clienteSlice = append(*clienteSlice, newClientes(nclientes, demora, terminar))
+			nclientes = nclientes + 1
+		}
 	}
-	*cli.terminado <- true
 }
 
 func main() {
+	var nclientes = 0
+	var clientes [2]*clientes
+	clienteSlice := clientes[0:2]
 
 }
 
-/*Deberá crear un framework de simulación con el que deberá programar un ejemplo de simulación de la atención a clientes en un banco (una cola de clientes con múltiples cajeros). En la figura 3 se aprecia una corrutina generadora de clientes, que permite representar sus llegadas al banco. Por otra parte se tiene c cajeros, a los que atienden a los clientes que están en la fila. Los clientes, una vez atendidos, se retiran del banco.
+/*
+	for i := 0; i < max; i++ {
+		fmt.Println("El cliente ", i)
+		cli.TiempoAtencionCli=1
+	}
+
+	*cli.terminado <- true
+*/
+
+/*Deberá crear un framework de simulación con el que deberá programar un ejemplo de simulación
+de la atención a clientes en un banco (una cola de clientes con múltiples cajeros). En la
+figura 3 se aprecia una corrutina generadora de clientes, que permite representar sus llegadas
+al banco. Por otra parte se tiene c cajeros, a los que atienden a los clientes que están en la fila. Los clientes, una vez atendidos, se retiran del banco.
 I)Cajeros cuya pausa en la ejecución permitirá simular el tiempo de atención al cliente
-II) Generador de clientes que simulará la creación de clientes, cuya pausa permite simular el tiempo que transcurre entre el arribo de un cliente y otro. Para la ejecución de la simulación se debe iterar de forma cíclica sobre una estructura que contiene referencias a las corrutinas antes mencionadas. En cada iteración se determinará aleatoriamente (con diferente probabilidad) si a la corrutina se la despierta o no, representando de esta forma que los clientes llegan a intervalos diferentes de tiempo y que las atenciones de los cajeros pueden variar en tiempo también.*/
+II) Generador de clientes que simulará la creación de clientes, cuya pausa permite simular el
+tiempo que transcurre entre el arribo de un cliente y otro. Para la ejecución de la simulación
+se debe iterar de forma cíclica sobre una estructura que contiene referencias a las corrutinas
+antes mencionadas. En cada iteración se determinará aleatoriamente (con diferente probabilidad)
+si a la corrutina se la despierta o no, representando de esta forma que los clientes llegan a
+intervalos diferentes de tiempo y que las atenciones de los cajeros pueden variar en tiempo
+también.*/
